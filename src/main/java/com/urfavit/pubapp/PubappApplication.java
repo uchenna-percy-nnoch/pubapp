584c6ede5e2761d36581bc075e2170693424f3e2
uchepercynnoch <uchennoch@gmail.com>
2023-02-16 14:48:16 +0100

    First commit

---
+++ src/main/java/com/urfavit/pubapp/PubappApplication.java
@@ -0,0 +1,13 @@
+package com.urfavit.pubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/urfavit/pubapp/PubappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urfavit.pubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
